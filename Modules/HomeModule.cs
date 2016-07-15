f6212010d86821a98b18e689f9a92c97676c6b8b
George Olson <georgeolson92@gmail.com>
2016-07-15 14:51:38 -0700

    Add ability to edit the names of clients and stylists

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -57,11 +57,59 @@ namespace HairSalon
         model.Add("clients", allClients);
         return View["index.cshtml", model];
       };
-      Delete["/clients/delete"] = p_ => {
-        int searchId = Request.Form["clientName"];
-        Client SelectedClient = Client.Find(searchId);
+      Delete["/clients/delete/all"] = _ => {
+        Client.DeleteAll();
+        return View["removed_clients.cshtml"];
+      };
+      Delete["/stylists/{id}/clients/delete"] = parameters => {
+        int deleteId = Request.Form["clientName"];
+        Client SelectedClient = Client.Find(deleteId);
         SelectedClient.Delete();
-        return View["removed_client.cshtml", SelectedClient];
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var searchId = parameters.id;
+        var SelectedStylist = Stylist.Find(searchId);
+        var StylistClients = SelectedStylist.GetClients();
+        model.Add("stylist", SelectedStylist);
+        model.Add("clients", StylistClients);
+        return View["stylist.cshtml", model];
+      };
+      Post["/stylists/edit/"] = _ => {
+        int searchId = Request.Form["stylistName"];
+        var selectedStylist = Stylist.Find(searchId);
+        return View["stylist_edit.cshtml", selectedStylist];
+      };
+      Post["/stylists/{id}/clients/edit"] = parameters => {
+        int stylistId = parameters.id;
+        var SelectedStylist = Stylist.Find(stylistId);
+        int clientId = Request.Form["clientName"];
+        Client SelectedClient = Client.Find(clientId);
+        Dictionary<string, object> modelEdit = new Dictionary<string, object>();
+        modelEdit.Add("stylist", SelectedStylist);
+        modelEdit.Add("client", SelectedClient);
+        return View["client_edit.cshtml", modelEdit];
+      };
+      Patch["/stylists/{id}/edit"] = parameters => {
+        int stylistId = parameters.id;
+        string newName = Request.Form["newName"];
+        var SelectedStylist = Stylist.Find(stylistId);
+        SelectedStylist.Update(newName);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var StylistClients = SelectedStylist.GetClients();
+        model.Add("stylist", SelectedStylist);
+        model.Add("clients", StylistClients);
+        return View["stylist.cshtml", model];
+      };
+      Patch["/stylists/{id}/clients/edit"] = parameters => {
+        int editId = Request.Form["clientEditId"];
+        string editName = Request.Form["newName"];
+        var selectedClient = Client.Find(editId);
+        selectedClient.Update(editName);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var SelectedStylist = Stylist.Find(parameters.id);
+        var StylistClients = SelectedStylist.GetClients();
+        model.Add("stylist", SelectedStylist);
+        model.Add("clients", StylistClients);
+        return View["stylist.cshtml", model];
       };
       // Get["/clients/edit/"] = _ => {
       //   return View["client_edit.cshtml"];
