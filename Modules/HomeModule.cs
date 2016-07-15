19bd8e922fb243986478b7510042a99c4a5c70c0
George Olson <georgeolson92@gmail.com>
2016-07-15 12:51:57 -0700

    Add ability to delete stylists, show message when nothing to be displayed in lists on Stylist & Client pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -17,6 +17,14 @@ namespace HairSalon
         model.Add("clients", allClients);
         return View["index.cshtml", model];
       };
+      Get["/stylists/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var SelectedStylist = Stylist.Find(parameters.id);
+        var StylistClients = SelectedStylist.GetClients();
+        model.Add("stylist", SelectedStylist);
+        model.Add("clients", StylistClients);
+        return View["stylist.cshtml", model];
+      };
       Post["/stylists/new"] = _ => {
         Stylist newStylist = new Stylist(Request.Form["stylistName"]);
         newStylist.Save();
@@ -38,14 +46,31 @@ namespace HairSalon
         model.Add("clients", StylistClients);
         return View["stylist.cshtml", model];
       };
-      Get["/stylists/{id}"] = parameters => {
-        Dictionary<string, object> model = new Dictionary<string, object>();
-        var SelectedStylist = Stylist.Find(parameters.id);
-        var StylistClients = SelectedStylist.GetClients();
-        model.Add("stylist", SelectedStylist);
-        model.Add("clients", StylistClients);
-        return View["stylist.cshtml", model];
+      Delete["/stylists/delete/"] = _ => {
+        int searchId = Request.Form["stylistName"];
+        Stylist SelectedStylist = Stylist.Find(searchId);
+        SelectedStylist.Delete();
+        List<Stylist> allStylists = Stylist.GetAll();
+        List<Client> allClients = Client.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("stylists", allStylists);
+        model.Add("clients", allClients);
+        return View["index.cshtml", model];
       };
+      // Get["/clients/edit/"] = _ => {
+      //   return View["client_edit.cshtml"];
+      // };
+      // Patch["/clients/edit"] = _ => {
+      //   int searchId = Request.Form["clientName"];
+      //   Client SelectedClient = Client.Find(searchId);
+      //   SelectedClient.Update(Request.Form["newName"]);
+      //   List<Stylist> allStylists = Stylist.GetAll();
+      //   List<Client> allClients = Client.GetAll();
+      //   Dictionary<string, object> model = new Dictionary<string, object> {};
+      //   model.Add("stylists", allStylists);
+      //   model.Add("clients", allClients);
+      //   return View["stylist.cshtml", model];
+      // }
     }
   }
 }
